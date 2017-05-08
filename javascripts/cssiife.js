f873abc42022ffb8baefb8d13219ac1e12c6a93a
Azim <azimsadikov@gmail.com>
2017-05-08 13:10:12 -0500

    added enter key event besides button now user can click button and press enter

--- javascripts/cssiife.js
+++ javascripts/cssiife.js
@@ -1,28 +1,30 @@
 "use strict"
-var CarLot = (function (original) {
-  var userInput = document.getElementById('input');
-  var currentTargetInfo = '';
-  var description = '';
+var CarLot = (function(original) {
+    var userInput = document.getElementById('input');
+    var currentTargetInfo = '';
+    var description = '';
     original.addBorder = function(event, color) {
         currentTargetInfo = event.currentTarget;
         // console.log('Watch:', currentTargetInfo)
         description = currentTargetInfo.querySelector('.description');
-       	// console.log('Watch:', abc.innerHTML)
-       	userInput.addEventListener('keyup', () => {
-       		description.innerHTML = '';
-       		description.innerHTML += userInput.value;
-       	});
+        // console.log('Watch:', abc.innerHTML)
+        userInput.addEventListener('keyup', (e) => {
+            description.innerHTML = '';
+            description.innerHTML += userInput.value;
+            if (e.keyCode === 13) {
+                currentTargetInfo.classList.remove(color);
+                userInput.value = '';
+            }
+        });
         currentTargetInfo.classList.toggle(color);
         userInput.focus();
     }
     original.enterText = function(event, color) {
-      if (event.type === 'click') {
-      	 console.log('Watch for it', description)
-      	 currentTargetInfo.classList.remove(color);
-      	 userInput.value = '';
-      }
+        if (event.type === 'click') {
+            console.log('Watch for it', description)
+            currentTargetInfo.classList.remove(color);
+            userInput.value = '';
+        }
     }
     return original;
 })(CarLot || {});
-
-
